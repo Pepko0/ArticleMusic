9d17ac4f48b86c7b246dda7ee4909966a9f79b7c
Kacper Lewko <k.lewkosga@gmail.com>
2023-03-06 14:56:40 +0100

    deleted bad classes CSS

--- script/script.js
+++ script/script.js
@@ -5,14 +5,14 @@
     
     const toggleBackground = () => {
         const body = document.querySelector(".body");
-        const article__themeName = document.querySelector(".article__themeName");
+        const article__themeName = document.querySelector(".js-button__themeName");
         body.classList.toggle("body__otherColor");
         article__themeName.innerText = body.classList.contains("body__otherColor") ? "szare" : "białe";
         
     };
     
     const init = () => {
-        const buttonChangeColor = document.querySelector(".article__button");
+        const buttonChangeColor = document.querySelector(".js-button");
         buttonChangeColor.addEventListener("click", toggleBackground );
     
         welcome();
